e916bec4c02faa050d7dd2faa8bc7a8d319b18bd
Jakub Czechowski <jakub.czechowski12321@gmail.com>
2025-03-27 18:07:51 +0100

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -12,7 +12,7 @@ load_dotenv()
 
 
 # Konfiguracja bazy danych
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL", connect_args={"sslmode": "require"})
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
